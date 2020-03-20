fcaa9ed2aaa3e719f80b743df300d33134c05602
Andy <andy.aschwanden@gmail.com>
2020-03-19 17:11:58 -0800

    added more modifiers

--- data_sets/climate_forcing/prepare_modifier.sh
+++ data_sets/climate_forcing/prepare_modifier.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for T in -6 -5 -4 -3 -2 -1 0; do
+for T in -6 -5 -4 -3 -2 -1 0 0.5 1; do
     echo "Creating climate modifier with T = ${T}C"
     python create_modifier.py -T_max ${T} arctic_climate_modifier_${T}C.nc
 done
