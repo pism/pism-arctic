5dd02c3ccbd85a6ea7d797a8cdf514bf1ed7c0c1
Andy <andy.aschwanden@gmail.com>
2020-06-25 13:35:55 -0800

    new climate forcing: monthly instead of annual

--- data_sets/climate_forcing/prepare_snap.sh
+++ data_sets/climate_forcing/prepare_snap.sh
@@ -1,5 +1,6 @@
 #!opt/local/bin/bash
 
+export HDF5_USE_FILE_LOCKING=FALSE
 epsg=5936
 
 x_min=-5800000.0
@@ -15,7 +16,7 @@ y_min=-1920000.0
 y_max=-860000.0
 
 
-options='-overwrite -t_srs EPSG:5936 -r average -co FORMAT=NC4 -co COMPRESS=DEFLATE -co ZLEVEL=1'
+options='-overwrite -t_srs EPSG:5936 -r bilinear -co FORMAT=NC4 -co COMPRESS=DEFLATE -co ZLEVEL=1'
 grid=2000
 
 # # file=pr_decadal_summaries_AK_CAN_2km_CRU_TS31_historical.zip
@@ -32,35 +33,36 @@ grid=2000
 # # wget -nc http://data.snap.uaf.edu/data/IEM/Inputs/ancillary/elevation/$file
 # # unzip -f  $file
 
+start_year=1980
+end_year=2005
 
-for year in {2000..2009}; do
+for year in {1980..2005}; do
     for mon in 0{1..9} {10..12} ; do
         gdalwarp $options -te $x_min $y_min $x_max $y_max -tr $grid $grid pr/pr_total_mm_CRU_TS40_historical_${mon}_${year}.tif pr/pr_total_mm_CRU_TS40_historical_${mon}_${year}_${domain}.nc
         gdalwarp $options -te $x_min $y_min $x_max $y_max -tr $grid $grid tas/tas_mean_C_CRU_TS40_historical_${mon}_${year}.tif tas/tas_mean_C_CRU_TS40_historical_${mon}_${year}_${domain}.nc
-        cdo settaxis,${year}-${mon}-1 -setreftime,2000-1-1  pr/pr_total_mm_CRU_TS40_historical_${mon}_${year}_${domain}.nc  pr/pr_total_mm_CRU_TS40_historical_${mon}_${year}_cdo_${domain}.nc
-        cdo settaxis,${year}-${mon}-1 -setreftime,2000-1-1  tas/tas_mean_C_CRU_TS40_historical_${mon}_${year}_${domain}.nc  tas/tas_mean_C_CRU_TS40_historical_${mon}_${year}_cdo_${domain}.nc
+        cdo settaxis,${year}-${mon}-1 -setreftime,${start_year}-1-1  pr/pr_total_mm_CRU_TS40_historical_${mon}_${year}_${domain}.nc  pr/pr_total_mm_CRU_TS40_historical_${mon}_${year}_cdo_${domain}.nc
+        cdo settaxis,${year}-${mon}-1 -setreftime,${start_year}-1-1  tas/tas_mean_C_CRU_TS40_historical_${mon}_${year}_${domain}.nc  tas/tas_mean_C_CRU_TS40_historical_${mon}_${year}_cdo_${domain}.nc
     done
 done
 
+exit
+
 gdalwarp $options -te $x_min $y_min $x_max $y_max -tr $grid $grid AKCanada_2km_DEM_mosaic.tif ${domain}_AKCanada_2km_DEM_mosaic_${domain}.nc
 
-cdo setattribute,precipitation@units="kg m-2 year-1",precipitation@standard_name="precipitation_flux" -chname,Band1,precipitation -mergetime pr/pr_total_mm_CRU_TS40_historical_*_*_cdo_${domain}.nc  pr_total_mm_CRU_TS40_historical_2000_2009_${domain}_MM.nc
-cdo setattribute,air_temp@units="degree_Celsius" -selvar,air_temp -chname,Band1,air_temp -mergetime tas/tas_mean_C_CRU_TS40_historical_*_*_cdo_${domain}.nc tas/tas_mean_C_CRU_TS40_historical_2000_2009_${domain}_MM.nc
+cdo setattribute,precipitation@units="kg m-2 year-1",precipitation@standard_name="precipitation_flux" -chname,Band1,precipitation -mulc,12 -mergetime pr/pr_total_mm_CRU_TS40_historical_*_*_cdo_${domain}.nc  pr_total_mm_CRU_TS40_historical_${start_year}_${end_year}_${domain}_MM.nc
+cdo setattribute,air_temp@units="degree_Celsius" -selvar,air_temp -chname,Band1,air_temp -mergetime tas/tas_mean_C_CRU_TS40_historical_*_*_cdo_${domain}.nc tas_mean_C_CRU_TS40_historical_${start_year}_${end_year}_${domain}_MM.nc
 
 cdo setattribute,usurf@units="m",usurf@standard_name="surface_elevation" -chname,Band1,usurf  ${domain}_AKCanada_2km_DEM_mosaic_${domain}.nc usurf_akcanada_2km_dem_${domain}.nc
 
-cdo -O -f nc4 -z zip_3 setmisstoc,0 -merge usurf_akcanada_2km_dem_${domain}.nc -selyear,2000/2009 pr_total_mm_CRU_TS40_historical_2000_2009_${domain}_MM.nc  -selyear,2000/2009 tas/tas_mean_C_CRU_TS40_historical_2000_2009_${domain}_MM.nc ${domain}_climate_cru_TS40_historical_2000_2009_MM.nc
-adjust_timeline.py -a 2000-1-1 -d 2000-1-1 -p monthly ${domain}_climate_cru_TS40_historical_2000_2009_MM.nc
+cdo -O -f nc4 -z zip_3 setmisstoc,0 -merge usurf_akcanada_2km_dem_${domain}.nc -selyear,${start_year}/${end_year} pr_total_mm_CRU_TS40_historical_${start_year}_${end_year}_${domain}_MM.nc  -selyear,${start_year}/${end_year} tas_mean_C_CRU_TS40_historical_${start_year}_${end_year}_${domain}_MM.nc ${domain}_climate_cru_TS40_historical_${start_year}_${end_year}_MM.nc
+adjust_timeline.py -a ${start_year}-1-1 -d ${start_year}-1-1 -p monthly ${domain}_climate_cru_TS40_historical_${start_year}_${end_year}_MM.nc
+cdo -O -f nc4 -z zip_1 merge usurf_akcanada_2km_dem_${domain}.nc -selvar,precipitation,air_temp -ymonmean ${domain}_climate_cru_TS40_historical_${start_year}_${end_year}_MM.nc ${domain}_climate_cru_TS40_historical_${start_year}_${end_year}_YMM.nc
+adjust_timeline.py -a 1992-1-1 -d 1992-1-1 -p monthly  ${domain}_climate_cru_TS40_historical_${start_year}_${end_year}_YMM.nc
 
-cdo -O -f nc4 -z zip_3 merge -setattribute,air_temp_sd@units="K" -yearstd -chname,air_temp,air_temp_sd -selvar,air_temp ${domain}_climate_cru_TS40_historical_2000_2009_MM.nc -yearmean -selvar,air_temp,usurf ${domain}_climate_cru_TS40_historical_2000_2009_MM.nc -yearsum -selvar,precipitation ${domain}_climate_cru_TS40_historical_2000_2009_MM.nc ${domain}_climate_cru_TS40_historical_2000_2009_YM.nc
-cdo -O -f nc4 -z zip_3 timmean -selyear,2000/2009 ${domain}_climate_cru_TS40_historical_2000_2009_YM.nc  ${domain}_climate_cru_TS40_historical_2000_2009_TM.nc
+cdo -O -f nc4 -z zip_3 timmean -selyear,${start_year}/${end_year} ${domain}_climate_cru_TS40_historical_${start_year}_${end_year}_MM.nc  ${domain}_climate_cru_TS40_historical_${start_year}_${end_year}_TM.nc
 
-gdal_translate NETCDF:${domain}_climate_cru_TS40_historical_2000_2009_TM.nc:precipitation ${domain}_climate_cru_TS40_historical_2000_2009_TM_precip.tif
-gdal_translate NETCDF:${domain}_climate_cru_TS40_historical_2000_2009_TM.nc:air_temp ${domain}_climate_cru_TS40_historical_2000_2009_TM_air_temp.tif
-gdal_translate NETCDF:${domain}_climate_cru_TS40_historical_2000_2009_TM.nc:air_temp_sd ${domain}_climate_cru_TS40_historical_2000_2009_TM_air_temp_sd.tif
-gdal_translate NETCDF:${domain}_climate_cru_TS40_historical_2000_2009_TM.nc:usurf ${domain}_climate_cru_TS40_historical_2000_2009_TM_usur.tif
+gdal_translate NETCDF:${domain}_climate_cru_TS40_historical_${start_year}_${end_year}_TM.nc:precipitation ${domain}_climate_cru_TS40_historical_${start_year}_${end_year}_TM_precip.tif
+gdal_translate NETCDF:${domain}_climate_cru_TS40_historical_${start_year}_${end_year}_TM.nc:air_temp ${domain}_climate_cru_TS40_historical_${start_year}_${end_year}_TM_air_temp.tif
+gdal_translate NETCDF:${domain}_climate_cru_TS40_historical_${start_year}_${end_year}_TM.nc:air_temp_sd ${domain}_climate_cru_TS40_historical_${start_year}_${end_year}_TM_air_temp_sd.tif
+gdal_translate NETCDF:${domain}_climate_cru_TS40_historical_${start_year}_${end_year}_TM.nc:usurf ${domain}_climate_cru_TS40_historical_${start_year}_${end_year}_TM_usurf.tif
 
-
-# Created Merged Climate
-
-cdo -O -f nc4 merge -selvar,precipitation akglaciers_climate_cru_TS40_historical_2000_2009_TM.nc -selvar,air_temp,air_temp_sd,usurf akglaciers_climate_MERRA2_2000_2009_TM.nc akglaciers_climate_m2_cru_2000_2009_TM.nc
--- initialization/run_ak.sh
+++ initialization/run_ak.sh
@@ -70,13 +70,42 @@ odir=2020_05_cru
 
 python initialization.py -e alaska.csv -s pleiades_broadwell -q long -w 8:00:00  --domain akglaciers -g ${grid} -n${n} --duration ${duration} --step ${duration} --o_dir ${odir} --exstep 100
 
+n=84
+grid=5000
+duration=20000
+odir=2020_05_sensitivity
+
+python initialization.py -e alaska.csv -s pleiades_broadwell -q long -w 8:00:00  --domain akglaciers -g ${grid} -n${n} --duration ${duration} --step ${duration} --o_dir ${odir} --exstep 100
+
 
 n=140
 grid=2000
 duration=5000
-odir=2020_05_m1
+step=2500
+odir=2020_05_sensitivity
 
-python initialization.py -e alaska.csv -s pleiades_broadwell -q long -w 80:00:00  --domain akglaciers -g ${grid} -n${n} --duration ${duration} --step ${duration} --o_dir ${odir} --exstep 100 -i 2020_04_cru/state/akglaciers_g2000m_id_CRU-DEF-1_0_10000_backup.nc
+for T in -5; do
+    for w in 3 5 7; do
+        python initialization.py -e alaska.csv -s pleiades_broadwell -q long -w 68:00:00  --domain akglaciers -g ${grid} -n${n} --duration ${duration} --step ${step} --o_dir ${odir} --exstep 100 -i  2020_05_sensitivity/state/akglaciers_g5000m_id_CRU-DEF${T}_P_${w}_0_20000.nc
+        qsub /nobackupp8/aaschwan/pism-arctic/initialization/2020_05_sensitivity/run_scripts/run_g${grid}m_id_CRU-DEF${T}_P_${w}_j.sh
+    done
+done
+
+n=140
+grid=1000
+duration=100
+step=100
+odir=2020_05_sensitivity
+
+for T in -5; do
+    for w in 3; do
+        python initialization.py -e alaska.csv -s pleiades_broadwell -q long -w 36:00:00  --domain akglaciers -g ${grid} -n${n} --duration ${duration} --step ${step} --o_dir ${odir} --exstep 100 -i  2020_05_sensitivity/state/akglaciers_g2000m_id_CRU-DEF${T}_P_${w}_2500_5000.nc
+        qsub /nobackupp8/aaschwan/pism-arctic/initialization/2020_05_sensitivity/run_scripts/run_g${grid}m_id_CRU-DEF${T}_P_${w}_j.sh
+    done
+done
+
+
+
 
 n=140
 grid=2000
