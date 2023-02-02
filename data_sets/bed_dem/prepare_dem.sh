47e223719332702000f243235b6709e7c8d8ae40
Andy Aschwanden <andy.aschwanden@alaska.edu>
2023-02-02 10:23:41 -0900

    removed lapse rate for LGM simulations

--- data_sets/bed_dem/prepare_dem.sh
+++ data_sets/bed_dem/prepare_dem.sh
@@ -24,7 +24,7 @@ y_max=-860000.0
 
 for grid in 250 500 1000 2000 5000; do
     # Surface from GEBCO
-    gdalwarp $options -dstnodata -9999 -s_srs EPSG:4326 -t_srs EPSG:${epsg} -te $x_min $y_min $x_max $y_max  -tr $grid $grid gebco_2021_n90.0_s45.0_w-180.0_e0.0.nc pism_${domain}_v${v}_g${grid}m.nc
+    gdalwarp $options -dstnodata -9999 -s_srs EPSG:4326 -t_srs EPSG:${epsg} -te $x_min $y_min $x_max $y_max  -tr $grid $grid GEBCO_2022.nc pism_${domain}_v${v}_g${grid}m.nc
     ncrename -v Band1,surface  pism_${domain}_v${v}_g${grid}m.nc
     ncatted -a _FillValue,surface,d,, -a standard_name,surface,o,c,"surface_altitude"  pism_${domain}_v${v}_g${grid}m.nc
     
@@ -65,9 +65,9 @@ y_min=-5800000
 x_max=9800000
 y_max=9800000
 
-for grid in 250 500 1000 2000 5000 10000 20000 40000; do
+for grid in 250; do
     # Surface from GEBCO
-    gdalwarp $options -dstnodata -9999 -s_srs EPSG:4326 -t_srs EPSG:${epsg} -te $x_min $y_min $x_max $y_max  -tr $grid $grid gebco_2021_n90.0_s45.0_w-180.0_e0.0.nc pism_${domain}_v${v}_g${grid}m.nc
+    gdalwarp $options -dstnodata -9999 -s_srs EPSG:4326 -t_srs EPSG:${epsg} -te $x_min $y_min $x_max $y_max  -tr $grid $grid GEBCO_2022.nc pism_${domain}_v${v}_g${grid}m.nc
     ncrename -v Band1,surface  pism_${domain}_v${v}_g${grid}m.nc
     ncatted -a _FillValue,surface,d,, -a standard_name,surface,o,c,"surface_altitude"  pism_${domain}_v${v}_g${grid}m.nc
     
@@ -96,4 +96,40 @@ for grid in 250 500 1000 2000 5000 10000 20000 40000; do
 done
 }
 
-akglaciers_grid
+alaska_grid() {
+
+domain=alaska
+
+x_min=-0
+y_min=-2000000
+x_max=5800000
+y_max=0
+
+for grid in 500; do
+    # Surface from GEBCO
+    gdalwarp $options -dstnodata -9999 -s_srs EPSG:4326 -t_srs EPSG:${epsg} -te $x_min $y_min $x_max $y_max  -tr $grid $grid GEBCO_2022.nc pism_${domain}_v${v}_g${grid}m.nc
+    ncrename -v Band1,surface  pism_${domain}_v${v}_g${grid}m.nc
+    ncatted -a _FillValue,surface,d,, -a standard_name,surface,o,c,"surface_altitude"  pism_${domain}_v${v}_g${grid}m.nc
+    
+
+    # Alaska Ice Thickness (Millan)
+    gdalwarp $options  -dstnodata 0 -te $x_min $y_min $x_max $y_max -tr $grid $grid ${icethicknessdir}/THICKNESS-RGI-merged_EPSG_${epsg}.vrt pism_${domain}_v${v}_g${grid}m_thickness.nc
+    ncrename -v Band1,thickness pism_${domain}_v${v}_g${grid}m_thickness.nc
+    ncatted -a _FillValue,thickness,d,,  pism_${domain}_v${v}_g${grid}m_thickness.nc
+    ncks -A -v thickness pism_${domain}_v${v}_g${grid}m_thickness.nc  pism_${domain}_v${v}_g${grid}m.nc
+
+    # Generate bed and ice thickness
+    ncap2 -O -s "topg=surface-thickness; ftt_mask=topg*0 + 1; where(thickness<0) {thickness=0;};"  pism_${domain}_v${v}_g${grid}m.nc  pism_${domain}_v${v}_g${grid}m.nc
+    ncatted  -a standard_name,ftt_mask,d,, -a standard_name,topg,o,c,"bedrock_altitude" -a standard_name,thickness,o,c,"land_ice_thickness" -a _FillValue,topg,d,, -a _FillValue,thickness,d,, -a units,thickness,o,c,"m" -a units,topg,o,c,"m" -a units,surface,o,c,"m" -a units,ftt_mask,d,, pism_${domain}_v${v}_g${grid}m.nc
+
+    ncap2 -O -s "where(surface<0) surface=0;" pism_${domain}_v${v}_g${grid}m.nc ${domain}_v${v}_g${grid}m.nc
+    ncap2 -O -s "where(thickness<10) surface=-9999;" pism_${domain}_v${v}_g${grid}m.nc ${domain}_v${v}_g${grid}m_surface_m.nc
+    cdo setmissval,-9999 ${domain}_v${v}_g${grid}m_surface_m.nc ${domain}_v${v}_g${grid}m_surface_masked.nc
+    for var in surface topg thickness; do
+        gdal_translate NETCDF:${domain}_v${v}_g${grid}m.nc:$var ${domain}_v${v}_g${grid}m_${var}.tif
+    done
+done
+}
+
+alaska_grid
+#akglaciers_grid
