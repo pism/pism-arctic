b24ebfdbc510eb357fef9dedc20016013c5df7a9
Andy Aschwanden <andy.aschwanden@alaska.edu>
2023-03-29 14:04:26 -0800

    work on climate forcing

--- data_sets/climate_forcing/prepare_snap.sh
+++ data_sets/climate_forcing/prepare_snap.sh
@@ -16,57 +16,52 @@ y_min=-1920000.0
 y_max=-860000.0
 
 
-options='-overwrite -t_srs EPSG:5936 -r bilinear -co FORMAT=NC4 -co COMPRESS=DEFLATE -co ZLEVEL=1'
+options='-overwrite -t_srs EPSG:5936 -r bilinear -co FORMAT=NC4 -co COMPRESS=DEFLATE -co ZLEVEL=2'
 grid=1000
 
-# # file=pr_decadal_summaries_AK_CAN_2km_CRU_TS31_historical.zip
-# file=pr_AK_CAN_2km_CRU_TS40_historical.zip
-# wget -nc http://data.snap.uaf.edu/data/Base/AK_CAN_2km/historical/CRU_TS/Historical_Monthly_and_Derived_Precipitation_Products_2km_CRU_TS/$file
-# unzip -f $file
+file=pr_total_mm_iem_cru_TS40_1901_2015.zip
+wget -nc http://data.snap.uaf.edu/data/IEM/Inputs/historical/precipitation/$file
+unzip -f $file
 
-# # file=tas_decadal_summaries_AK_CAN_2km_CRU_TS31_historical.zip
-# file=tas_AK_CAN_2km_CRU_TS40_historical.zip
-# wget -nc http://data.snap.uaf.edu/data/Base/AK_CAN_2km/historical/CRU_TS/Historical_Monthly_and_Derived_Temperature_Products_2km_CRU_TS/$file
-# unzip -f  $file
+file=tas_mean_C_iem_cru_TS40_1901_2015.zip
+wget -nc http://data.snap.uaf.edu/data/IEM/Inputs/historical/temperature/$file
+unzip -f  $file
 
-# # file=iem_prism_dem_1km.tif
-# # wget -nc http://data.snap.uaf.edu/data/IEM/Inputs/ancillary/elevation/$file
-# # unzip -f  $file
+dem_file=iem_prism_dem_1km.tif
+wget -nc http://data.snap.uaf.edu/data/IEM/Inputs/ancillary/elevation/$file
+unzip -f  $file
+gdalwarp $options -te $x_min $y_min $x_max $y_max -tr $grid $grid ${dem_file} ${domain}_iem_prism_dem_1km_${domain}.nc
 
 start_year=1980
 end_year=2004
 
+pr_prefix=pr_cru_TS40_1km
+tas_prefix=tas_cru_TS40_1km
+
 for year in {1980..2004}; do
     for mon in 0{1..9} {10..12} ; do
-        gdalwarp $options -te $x_min $y_min $x_max $y_max -tr $grid $grid pr_cru_TS40_1km/pr_total_mm_CRU_TS40_historical_${mon}_${year}.tif pr_cru_TS40_1km/pr_total_mm_CRU_TS40_historical_${mon}_${year}_${domain}.nc
-        gdalwarp $options -te $x_min $y_min $x_max $y_max -tr $grid $grid tas_cru_TS40_1km/tas_mean_C_CRU_TS40_historical_${mon}_${year}.tif tas_cru_TS40_1km/tas_mean_C_CRU_TS40_historical_${mon}_${year}_${domain}.nc
-        cdo -L settaxis,${year}-${mon}-1 -setreftime,${start_year}-1-1  pr_cru_TS40_1km/pr_total_mm_CRU_TS40_historical_${mon}_${year}_${domain}.nc  pr_cru_TS40_1km/pr_total_mm_CRU_TS40_historical_${mon}_${year}_cdo_${domain}.nc
-        cdo -L settaxis,${year}-${mon}-1 -setreftime,${start_year}-1-1  tas_cru_TS40_1km/tas_mean_C_CRU_TS40_historical_${mon}_${year}_${domain}.nc  tas_cru_TS40_1km/tas_mean_C_CRU_TS40_historical_${mon}_${year}_cdo_${domain}.nc
+        gdalwarp $options -te $x_min $y_min $x_max $y_max -tr $grid $grid ${pr_prefix}/pr_total_mm_CRU_TS40_historical_${mon}_${year}.tif ${pr_prefix}/pr_total_mm_CRU_TS40_historical_${mon}_${year}_${domain}.nc
+        gdalwarp $options -te $x_min $y_min $x_max $y_max -tr $grid $grid ${tas_prefix}/tas_mean_C_CRU_TS40_historical_${mon}_${year}.tif ${tas_prefix}/tas_mean_C_CRU_TS40_historical_${mon}_${year}_${domain}.nc
+        cdo -L settaxis,${year}-${mon}-1 -setreftime,${start_year}-1-1  ${pr_prefix}/pr_total_mm_CRU_TS40_historical_${mon}_${year}_${domain}.nc  ${pr_prefix}/pr_total_mm_CRU_TS40_historical_${mon}_${year}_cdo_${domain}.nc
+        cdo -L settaxis,${year}-${mon}-1 -setreftime,${start_year}-1-1  ${tas_prefix}/tas_mean_C_CRU_TS40_historical_${mon}_${year}_${domain}.nc  ${tas_prefix}/tas_mean_C_CRU_TS40_historical_${mon}_${year}_cdo_${domain}.nc
     done
 done
 
 
-gdalwarp $options -te $x_min $y_min $x_max $y_max -tr $grid $grid iem_prism_dem_1km.tif ${domain}_iem_prism_dem_1km_${domain}.nc
 
-cdo -L setattribute,precipitation@units="kg m-2 year-1",precipitation@standard_name="precipitation_flux" -chname,Band1,precipitation -mulc,12 -mergetime pr/pr_total_mm_CRU_TS40_historical_*_*_cdo_${domain}.nc  pr_total_mm_CRU_TS40_historical_${start_year}_${end_year}_${domain}_MM.nc
-cdo -L setattribute,air_temp@units="degree_Celsius" -selvar,air_temp -chname,Band1,air_temp -mergetime tas/tas_mean_C_CRU_TS40_historical_*_*_cdo_${domain}.nc tas_mean_C_CRU_TS40_historical_${start_year}_${end_year}_${domain}_MM.nc
+cdo -L setattribute,precipitation@units="kg m-2 year-1",precipitation@standard_name="precipitation_flux",precipitation@long_name="precipitation" -chname,Band1,precipitation -mulc,12 -mergetime ${pr_prefix}/pr_total_mm_CRU_TS40_historical_*_*_cdo_${domain}.nc  pr_total_mm_CRU_TS40_historical_${start_year}_${end_year}_${domain}_MM.nc
+cdo -L setattribute,air_temp@units="degree_Celsius",air_temp@long_name="2-m air temperature" -selvar,air_temp -chname,Band1,air_temp -mergetime ${tas_prefix}/tas_mean_C_CRU_TS40_historical_*_*_cdo_${domain}.nc tas_mean_C_CRU_TS40_historical_${start_year}_${end_year}_${domain}_MM.nc
 
-cdo -L setattribute,usurf@units="m",usurf@standard_name="surface_elevation" -chname,Band1,usurf  ${domain}_iem_prism_dem_1km_${domain}.nc usurf_akcanada_1km_dem_${domain}.nc
+cdo -L setattribute,usurf@units="m",usurf@standard_name="surface_elevation",usurf@long_name="surface elevation" -chname,Band1,usurf  ${domain}_iem_prism_dem_1km_${domain}.nc usurf_akcanada_1km_dem_${domain}.nc
 
-cdo -L -O -f nc4 -z zip_3 setmisstoc,0 -merge usurf_akcanada_1km_dem_${domain}.nc -selyear,${start_year}/${end_year} pr_total_mm_CRU_TS40_historical_${start_year}_${end_year}_${domain}_MM.nc  -selyear,${start_year}/${end_year} tas_mean_C_CRU_TS40_historical_${start_year}_${end_year}_${domain}_MM.nc ${domain}_climate_cru_TS40_historical_${start_year}_${end_year}_MM.nc
+cdo -L -O -f nc4 -z zip_2 merge usurf_akcanada_1km_dem_${domain}.nc -selyear,${start_year}/${end_year} pr_total_mm_CRU_TS40_historical_${start_year}_${end_year}_${domain}_MM.nc  -selyear,${start_year}/${end_year} tas_mean_C_CRU_TS40_historical_${start_year}_${end_year}_${domain}_MM.nc ${domain}_climate_cru_TS40_historical_${start_year}_${end_year}_MM.nc
 adjust_timeline.py -a ${start_year}-1-1 -d ${start_year}-1-1 -p monthly ${domain}_climate_cru_TS40_historical_${start_year}_${end_year}_MM.nc
 
-cdo -L -O -f nc4 -z zip_1 setmisstoc,0 -merge usurf_akcanada_1km_dem_${domain}.nc -selvar,precipitation,air_temp -ymonmean ${domain}_climate_cru_TS40_historical_${start_year}_${end_year}_MM.nc ${domain}_climate_cru_TS40_historical_${start_year}_${end_year}_YMM.nc
+cdo -L -O -f nc4 -z zip_2 selvar,precipitation,air_temp -ymonmean ${domain}_climate_cru_TS40_historical_${start_year}_${end_year}_MM.nc ${domain}_climate_cru_TS40_historical_${start_year}_${end_year}_YMM_uf.nc
+mpirun -np 8 fill_missing_petsc.py -v air_temp,precipitation ${domain}_climate_cru_TS40_historical_${start_year}_${end_year}_YMM_uf.nc ${domain}_climate_cru_TS40_historical_${start_year}_${end_year}_YMM.nc
 adjust_timeline.py -a 1-1-1 -d 1-1-1 -p monthly -c 365_day ${domain}_climate_cru_TS40_historical_${start_year}_${end_year}_YMM.nc
-
-cdo -L -O -f nc4 -z zip_1 yseasmean ${domain}_climate_cru_TS40_historical_${start_year}_${end_year}_MM.nc ${domain}_climate_cru_TS40_historical_${start_year}_${end_year}_YSM.nc
-
-
-cdo -L -O -f nc4 -z zip_3 timmean -selyear,${start_year}/${end_year} ${domain}_climate_cru_TS40_historical_${start_year}_${end_year}_MM.nc  ${domain}_climate_cru_TS40_historical_${start_year}_${end_year}_TM.nc
-cdo -L -O -f nc4 -z zip_3 setattribute,precipitation@units="m" -divc,1000 -selvar,precipitation ${domain}_climate_cru_TS40_historical_${start_year}_${end_year}_TM.nc ${domain}_precipitation_cru_TS40_historical_${start_year}_${end_year}_TM.nc
-
-gdal_translate NETCDF:${domain}_climate_cru_TS40_historical_${start_year}_${end_year}_TM.nc:precipitation ${domain}_climate_cru_TS40_historical_${start_year}_${end_year}_TM_precip.tif
-gdal_translate NETCDF:${domain}_climate_cru_TS40_historical_${start_year}_${end_year}_TM.nc:air_temp ${domain}_climate_cru_TS40_historical_${start_year}_${end_year}_TM_air_temp.tif
-gdal_translate NETCDF:${domain}_climate_cru_TS40_historical_${start_year}_${end_year}_TM.nc:air_temp_sd ${domain}_climate_cru_TS40_historical_${start_year}_${end_year}_TM_air_temp_sd.tif
-gdal_translate NETCDF:${domain}_climate_cru_TS40_historical_${start_year}_${end_year}_TM.nc:usurf ${domain}_climate_cru_TS40_historical_${start_year}_${end_year}_TM_usurf.tif
+ncks -4 -L 2 -A usurf_akcanada_1km_dem_${domain}.nc ${domain}_climate_cru_TS40_historical_${start_year}_${end_year}_YMM.nc
+cdo setmisstoc,0 ${domain}_climate_cru_TS40_historical_${start_year}_${end_year}_YMM.nc tmp_${domain}_climate_cru_TS40_historical_${start_year}_${end_year}_YMM.nc
+mv tmp_${domain}_climate_cru_TS40_historical_${start_year}_${end_year}_YMM.nc ${domain}_climate_cru_TS40_historical_${start_year}_${end_year}_YMM.nc
+cdo -L -O -f nc4 -z zip_2 timmean ${domain}_climate_cru_TS40_historical_${start_year}_${end_year}_YMM.nc ${domain}_climate_cru_TS40_historical_${start_year}_${end_year}_TM.nc
 
